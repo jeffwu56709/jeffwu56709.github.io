13182d13df748de4c83662982632cb0abd654337
jeffwu56709 <114436102+jeffwu56709@users.noreply.github.com>
2025-04-24 02:18:08 -0400

    Update products.php
    final formatting changes

--- JQSM/Phase1/products.php
+++ JQSM/Phase1/products.php
@@ -1,4 +1,5 @@
 <?php include 'connect.php'; ?>
+<?php include 'navbar.html'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,20 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 <body style="background-color:darkslategrey;">
-  <div id="wrapper">
-      <div class="menu">
-        <img src="Assets/jefflogo.png" alt="Logo" class="logo">
-        <a class ="qsm">Quick Stop Mart</a>    
-        <a href="index.html" class ="link"><img src="Assets/home.png" alt="Home" width="50px" height="50px"></a>
-        <a href="categories.html" class ="link"><img src="Assets/category.png" alt="Categories" width="50px" height="50px"></a>
-        <form>
-            <input type="text" placeholder="Search Products.." class="searchbar" id="search" name="search">
-            <button type="submit" style="font-size:14px" class ="button"><img src="Assets/search.png" alt="Search" width="25px" height="25px"></button>
-        </form>
-        <a href="contact.html" class ="link"><img src="Assets/contact.png" alt="Contact Us" width="50px" height="50px"></a>
-        <a href="login.html" class ="link"><img src="Assets/login.png" alt="Log In" width="50px" height="50px"></a>
-      </div>
-    </div><br><br>
+  <br><br>
   <div class ="firstline">PRODUCTS</div><br>
   <div class ="sortselect">
      <select name="sorter" id="sorter">
@@ -33,15 +21,17 @@
   </div>
   <br>
     <?php
-      $sql = "SELECT * FROM product";
+      $sql = "SELECT * FROM products";
       $result = $conn->query($sql);
       echo "<div class='grid-row'>";
       while ($row = $result->fetch_assoc()) {
-        echo "<div class='grid-item'>";
-        echo "<img src='Assets/Placeholders/pc_fruits.jpg' alt='Category1'>";
-        echo "<div class='grid-category1-name'>{$row['product_name']}</div>";
-        echo "<div class='grid-category1-price'>$" . number_format($row['selling_price'], 2) . "</div>";
-        echo "</div>";
+        echo "<a href='product.php?id={$row['product_id']}'>";
+          echo "<div class='grid-item'>";
+          echo "<img src='/JQSM/Assets/" . $row['image'] . "' alt='" . $row['product_name'] . "'>";
+          echo "<div>{$row['product_name']}<br>";
+          echo "\${$row['selling_price']}</div>";
+          echo "</div>";
+        echo "</a>";
       }
       echo "</div>";
     ?>
