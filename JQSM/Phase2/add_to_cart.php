2580411e354a9485ba92b9f4c5518ddc354e2177
jeffwu56709 <114436102+jeffwu56709@users.noreply.github.com>
2025-05-09 03:18:12 -0400

    Update add_to_cart.php
    added login check

--- JQSM/Phase2/add_to_cart.php
+++ JQSM/Phase2/add_to_cart.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+    if (!isset($_SESSION['user_id'])) {
+      header("Location: login.php?login=required");
+      exit;
+    }
     $product_id = $_POST['product_id'];
     $quantity = $_POST['quantity'];
 
